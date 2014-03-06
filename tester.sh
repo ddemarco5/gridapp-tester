bd8c1051ae24df1129d08b8c0e5625bc6d815252
Dominic DeMarco <demarcd@clarkson.edu>
2014-03-06 00:16:19 -0500

    Changed to output the files to csv's
    the comments got lost somewhere along the way though.

--- tester.sh
+++ tester.sh
@@ -1,133 +1,105 @@
-#!/bin/bash
-
-#Mick Tarsel
-# Runs tests for gridadpp.c 
-# and saves into directory
-#   ./testing/thread-count/
-#   ./testing/grid-size/
-
-#Arguments for gridapp.c:
-# grid size, thread count
-
-#
-#To compile and run:
-#    chomd u+x ./tester.sh
-#    time ./tester.sh
-#
-#
-
-
-
-#mkdir -p ./testing/thread-count/
-#mkdir -p ./testing/grid-size/
+#!/bin/bash 
 
 clear
-
+ 
 echo "Sup, $USER"
-
+ 
 echo ""
 pwd
 echo ""
 
-#GRID : THREAD TESTING
+echo "Testing NONE with changing threads"
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -grid > ./testing/thread-count/grid_thread_count_$i.txt
+./gridapp 10 $i -none | grep elapsed | cut -d' ' -f4 >> ./none_count.csv
 done
 
-#GRID: GRID TESTING
+echo "Testing NONE with changing size"
 for i in 2 3 4 5 6 7 8 9 10; do
-	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -grid > ./testing/grid-size/grid_grid_size_$i.txt
+echo "Executing with grid size $i and 10 number of threads. "
+./gridapp $i 10 -none | grep elapsed | cut -d' ' -f4 >> ./none_size.csv
 done
-
 
 
 echo""
 echo""
 echo "FINISHED EXECUTING: "
 echo""
-echo " ./gridapp 10 [1-10] -grid " 
-echo " ./gridapp [1-10] 10 -grid " 
+echo " ./gridapp 10 [1-10] -none "
+echo " ./gridapp [1-10] 10 -none "
 echo "#####################"
 echo""
 echo""
 
-
-#CELL : THREAD TESTING
+ 
+echo "Testing GRID with changing thread"
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -cell > ./testing/thread-count/cell_thread_count_$i.txt
+./gridapp 10 $i -grid | grep elapsed | cut -d' ' -f4  >> ./grid_count.csv
 done
-
-#CELL : GRID TESTING
+ 
+echo "Testing GRID with changing size"
 for i in 2 3 4 5 6 7 8 9 10; do
-	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -cell > ./testing/grid-size/cell_grid_size_$i.txt
+echo "Executing with grid size $i and 10 number of threads. "
+./gridapp $i 10 -grid | grep elapsed | cut -d' ' -f4  >> ./grid_size.csv
 done
-
-
+ 
+ 
 echo""
 echo""
 echo "FINISHED EXECUTING: "
 echo""
-echo " ./gridapp 10 [1-10] -cell " 
-echo " ./gridapp [1-10] 10 -cell " 
+echo " ./gridapp 10 [1-10] -grid "
+echo " ./gridapp [1-10] 10 -grid "
 echo "#####################"
 echo""
-
-#ROW : THREAD TESTING
+echo"" 
+ 
+echo "Testing ROW with changing thread"
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -row > ./testing/thread-count/row_thread_count_$i.txt
+./gridapp 10 $i -row | grep elapsed | cut -d' ' -f4 >> ./row_count.csv
 done
-
-#ROW: GRID TESTING
+ 
+echo "Testing ROW with changing size"
 for i in 2 3 4 5 6 7 8 9 10; do
-	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -row > ./testing/grid-size/row_grid_size_$i.txt
+echo "Executing with grid size $i and 10 number of threads. "
+./gridapp $i 10 -row | grep elapsed | cut -d' ' -f4  >> ./row_size.csv
 done
-
-
+ 
+ 
 echo""
 echo""
 echo "FINISHED EXECUTING: "
 echo""
-echo " ./gridapp 10 [1-10] -row " 
-echo " ./gridapp [1-10] 10 -row " 
-echo "#####################"
-echo""
-echo""
-
-#NONE : THREAD TESTING
-for i in 1 2 3 4 5 6 7 8 9 10; do
-echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -none > ./testing/thread-count/none_thread_count_$i.txt
-done
-
-#NONE: GRID TESTING
-for i in 2 3 4 5 6 7 8 9 10; do
-	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -none > ./testing/grid-size/none_grid_size_$i.txt
-done
-
-echo""
-echo""
-echo "FINISHED EXECUTING: "
-echo""
-echo " ./gridapp 10 [1-10] -none " 
-echo " ./gridapp [1-10] 10 -none " 
+echo " ./gridapp 10 [1-10] -row "
+echo " ./gridapp [1-10] 10 -row "
 echo "#####################"
 echo""
 echo""
 echo" done."
+echo""
+
+
+echo "Testing CELL with changing thread"
+for i in 1 2 3 4 5 6 7 8 9 10; do
+echo "Executing with $i number of threads. Grid size 10 "
+./gridapp 10 $i -cell | grep elapsed | cut -d' ' -f4 >> ./cell_count.csv
+done
+ 
+echo "Testing CELL with changing size"
+for i in 2 3 4 5 6 7 8 9 10; do
+echo "Executing with grid size $i and 10 number of threads. "
+./gridapp $i 10 -cell | grep elapsed | cut -d' ' -f4 >> ./cell_size.csv
+done
+ 
+ 
+echo""
+echo""
+echo "FINISHED EXECUTING: "
+echo""
+echo " ./gridapp 10 [1-10] -cell "
+echo " ./gridapp [1-10] 10 -cell "
+echo "#####################"
 echo""
 
